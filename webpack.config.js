97d288f83ffcb6c7cc83b6f4d8d0cb13e1f00627
Vishnu linux <vishnulinux02@gmail.com>
2021-02-26 11:58:51 +0530

    test

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const babelLoader = {
+  test: /\.js$/,
+  exclude: /(node_modules)/,
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-env"]
+    }
+  }
+};
+
+module.exports = {
+  entry: "./index.js",
+  plugins: [new CleanWebpackPlugin("build")],
+  output: {
+    filename: "main.js",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [babelLoader]
+  },
+  optimization: {
+    minimize: false
+  },
+   mode: 'development',
+   externalsType: "import"
+};
